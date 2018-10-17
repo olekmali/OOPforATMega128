b751be2575321ab0c590d01be6ed8fd48d2e46df
olekmali <olekmali@ieee.org>
2018-10-16 22:10:49 -0500

    Demonstrated virtual inheritance in the PushButton inheritance homewokr assignment startup project

--- oop_buttons_homework/oop_buttons_homework/oop_buttons_homework.cpp
+++ oop_buttons_homework/oop_buttons_homework/oop_buttons_homework.cpp
@@ -13,22 +13,29 @@
 
 int main(void)
 {
+    // static constants - hidden global constants
+    static const uint8_t   keymask[4] = {  MenuKeys::Key4, MenuKeys::Key5, MenuKeys::Key6, MenuKeys::Key7  };
+    static const uint8_t   ledmask[4] = {  LEDs::LED0,     LEDs::LED1,     LEDs::LED2,     LEDs::LED3      };
+
     // INITIALIZATION
     MenuKeys Keys;
     LEDs     disp;
 
-    DebouncedTogglePushButton    buttons[4];
-    static const uint8_t   keymask[4] = {  MenuKeys::Key4, MenuKeys::Key5, MenuKeys::Key6, MenuKeys::Key7  };
-    static const uint8_t   ledmask[4] = {  LEDs::LED0,     LEDs::LED1,     LEDs::LED2,     LEDs::LED3      };
-
+    // four PushButtons of various types
+	TogglePushButton B0;
+	TogglePushButton B1;
+	DebouncedTogglePushButton B2;
+	DebouncedTogglePushButton B3;
+    PushButton * buttons[4] = { &B0, &B1, &B2, &B3};
+		
     uint16_t cnt = 0;
     // MAIN SUPERLOOP
     while(1)
     {
         for (uint8_t i=0; i<4; ++i) {
-            buttons[i].update( 0!= (Keys.get() & keymask[i]) );
-            if ( buttons[i].getState() )  disp.set( disp.get() | ledmask[i] );
-            else                          disp.set( disp.get() & ~(ledmask[i]) );
+            (*buttons[i]).update( 0!= (Keys.get() & keymask[i]) );
+            if ( (*buttons[i]).getState() )  disp.set( disp.get() | ledmask[i] );
+            else                             disp.set( disp.get() & ~(ledmask[i]) );
         }
 
         if (0==cnt) {

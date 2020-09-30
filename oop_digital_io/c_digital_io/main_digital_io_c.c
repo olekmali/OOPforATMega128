4e61a917e09ff5b402a6a10fb2ab5cc5e06f10e2
olekmali <olekmali@ieee.org>
2020-09-30 01:15:53 -0500

    Simplified the very first OOP example - digital IO for LEDs and buttons

--- oop_digital_io/c_digital_io/main_digital_io_c.c
+++ oop_digital_io/c_digital_io/main_digital_io_c.c
@@ -1,44 +1,25 @@
 //* Simple Digital IO test set in plain C *
 #include "bios_key4c.h"
 #include "bios_led8c.h"
-
 #include <stdint.h>
 
 # define F_CPU 16000000UL
 #include <util/delay.h>
 
 
-uint8_t choose_on_power_on(uint8_t wait) {
-    uint8_t     result=0;
-    do {
-        result = key4_get();
-    } while ( wait && (0==result) );
+void main_toggle() {
+    uint8_t  state = 0;
 
-    if (0!=result) {
-        uint8_t waitst=0;
-        while ( waitst<64 ) {
-            if ( 0 != key4_get() ) waitst=0;
-            else waitst++;
-        }
-    }
-    return(result);
-}
-
-
-
-void main_toggle1() {
-	uint8_t  state = 0;
-	
-	// MAIN SUPERLOOP
-	while(1)
-	{
-    	uint8_t current = key4_get();
+    // MAIN SUPERLOOP
+    while(1)
+    {
+        uint8_t current = key4_get();
         uint8_t change  = current ^ state;
         state = current;
-    	    if ( (0 != (change & B_K4)) && (0 != (current & B_K4)) ) led8_set( led8_get() ^ B_L0 );
-    	    if ( (0 != (change & B_K5)) && (0 != (current & B_K5)) ) led8_set( led8_get() ^ B_L1 );
-    	    if ( (0 != (change & B_K6)) && (0 != (current & B_K6)) ) led8_set( led8_get() ^ B_L2 );
-    	    if ( (0 != (change & B_K7)) && (0 != (current & B_K7)) ) led8_set( led8_get() ^ B_L3 );
+            if ( (0 != (change & B_K4)) && (0 != (current & B_K4)) ) led8_set( led8_get() ^ B_L0 );
+            if ( (0 != (change & B_K5)) && (0 != (current & B_K5)) ) led8_set( led8_get() ^ B_L1 );
+            if ( (0 != (change & B_K6)) && (0 != (current & B_K6)) ) led8_set( led8_get() ^ B_L2 );
+            if ( (0 != (change & B_K7)) && (0 != (current & B_K7)) ) led8_set( led8_get() ^ B_L3 );
 
         // let's do at least lousy debouncing
         _delay_ms(10); // from <util/delay.h>
@@ -47,67 +28,36 @@ void main_toggle1() {
 
 
 
-void main_menu1() {
-	uint8_t  state = 0;
+void main_menu() {
+    uint8_t  state = 0;
     uint8_t  mode  = 1;
     uint8_t  prev  = 0;
-	
-	// MAIN SUPERLOOP
-	while(1)
-	{
-    	uint8_t current = key4_get();
-    	uint8_t change  = current ^ state;
-    	state = current;
-    	if ( (0 != (change & B_K4)) && (0 != (current & B_K4)) ) { if (0x01<mode) mode=mode>>1; }
-    	if ( (0 != (change & B_K5)) && (0 != (current & B_K5)) ) {}
-    	if ( (0 != (change & B_K6)) && (0 != (current & B_K6)) ) {}
-    	if ( (0 != (change & B_K7)) && (0 != (current & B_K7)) ) { if (0x08>mode) mode=mode<<1; }
+
+    // MAIN SUPERLOOP
+    while(1)
+    {
+        uint8_t current = key4_get();
+        uint8_t change  = current ^ state;
+        state = current;
+        if ( (0 != (change & B_K4)) && (0 != (current & B_K4)) ) { if (0x01<mode) mode=mode>>1; }
+        if ( (0 != (change & B_K5)) && (0 != (current & B_K5)) ) {}
+        if ( (0 != (change & B_K6)) && (0 != (current & B_K6)) ) {}
+        if ( (0 != (change & B_K7)) && (0 != (current & B_K7)) ) { if (0x08>mode) mode=mode<<1; }
 
         if (mode!=prev) {
             led8_set( led8_get() ^ mode ^ prev );
-            prev = mode;            
+            prev = mode;
         }
 
-    	// let's do at least lousy debouncing
-    	_delay_ms(10); // from <util/delay.h>
-	}
-}
-
-
-void main_blank1() {
-    while (1) ;
-}
-
-
-void main_blank2() {
-    while (1) ;
+        // let's do at least lousy debouncing
+        _delay_ms(10); // from <util/delay.h>
+    }
 }
 
 
 int main(void)
 {
-    // INITIALIZATION
-    led8_init();
-    key4_init();
-
-    switch ( choose_on_power_on(1) ) {
-    case B_K4 :
-        led8_set(B_L5);
-        main_toggle1();
-        break;
-    case B_K5 :
-        led8_set(B_L5|B_L6);
-        main_menu1();
-        break;
-    case B_K6 :
-        led8_set(B_L5|B_L7);
-        main_blank1();
-        break;
-    case B_K7 : 
-        led8_set(B_L5|B_L6|B_L7);
-        main_blank2();
-        break;
-    }
-
-	return(0);
+    main_toggle();
+    // main_menu();
+    return(0);
 }
--- oop_digital_io/oop_digital_io/main_digital_io.cpp
+++ oop_digital_io/oop_digital_io/main_digital_io.cpp
@@ -1,124 +1,72 @@
-//*  Digital IO test set with OOP button examples *
+//* Simple Digital IO test set without further OOP *
 #include "bios_key4.h"
 #include "bios_led8.h"
-#include "lib_buttons.h"
-
 #include <stdint.h>
 
-
-
-uint8_t choose_on_power_on(bool wait=true) {
-    MenuKeys    Keys;
-    uint8_t     result=0;
-    do {
-        result = Keys.get();
-    } while ( wait && (0==result) );
-
-    if (0!=result) {
-        uint8_t waitst=0;
-        while ( waitst<64 ) {
-            if ( 0 != Keys.get() ) waitst=0; 
-            else waitst++;
-        }            
-    }
-    return(result);
-}
+# define F_CPU 16000000UL
+#include <util/delay.h>
 
 
 
-void main_toggle1() {
+void main_toggle() {
     // INITIALIZATION
     MenuKeys Keys;
     LEDs     disp;
 
-    DebounceFilter  ButtonFilter[4];
-    ToggleButton    buttons[4];
-    const uint8_t   keymask[4] = {  MenuKeys::Key4, MenuKeys::Key5, MenuKeys::Key6, MenuKeys::Key7  };
-    const uint8_t   ledmask[4] = {  LEDs::LED0,     LEDs::LED1,     LEDs::LED2,     LEDs::LED3      };
+    uint8_t  state = 0;
 
     // MAIN SUPERLOOP
     while(1)
     {
-        for (uint8_t i=0; i<4; ++i) {
-            ButtonFilter[i].update( 0!= (Keys.get() & keymask[i]) );
-            buttons[i].update( ButtonFilter[i].getState() );
-            if ( buttons[i].getState() )  disp.set( disp.get() | ledmask[i] );
-            else                          disp.set( disp.get() & ~(ledmask[i]) );
-        }
+        uint8_t current = Keys.get();
+        uint8_t change  = current ^ state;
+        state = current;
+            if ( (0 != (change & MenuKeys::Key4)) && (0 != (current & MenuKeys::Key4)) ) disp.set( disp.get() ^ LEDs::LED0 );
+            if ( (0 != (change & MenuKeys::Key5)) && (0 != (current & MenuKeys::Key5)) ) disp.set( disp.get() ^ LEDs::LED1 );
+            if ( (0 != (change & MenuKeys::Key6)) && (0 != (current & MenuKeys::Key6)) ) disp.set( disp.get() ^ LEDs::LED2 );
+            if ( (0 != (change & MenuKeys::Key7)) && (0 != (current & MenuKeys::Key7)) ) disp.set( disp.get() ^ LEDs::LED3 );
+
+        // let's do at least lousy debouncing
+        _delay_ms(10); // from <util/delay.h>
     }
 }
 
 
 
-void main_menu1() {
+void main_menu() {
     // INITIALIZATION
     MenuKeys Keys;
     LEDs     disp;
 
-    DebounceFilter     leftButton;
-    DebounceFilter     rightButton(false);
-    EdgeDetectorFilter left;
-    EdgeDetectorFilter right;
+    uint8_t  state = 0;
+    uint8_t  mode  = 1;
+    uint8_t  prev  = 0;
 
     // MAIN SUPERLOOP
-    uint8_t mode = 1;
-    uint8_t prev = 0;
     while(1)
     {
-        leftButton.update(    0!= (Keys.get() & MenuKeys::Key4) );
-        rightButton.update(   0!= (Keys.get() & MenuKeys::Key7) );
-        left.update(  leftButton.getState() );
-        right.update( rightButton.getState() );
-
-        if ( left.getState() )  { if (0x01<mode) mode=mode>>1; }
-        if ( right.getState() ) { if (0x08>mode) mode=mode<<1; }
+        uint8_t current = Keys.get();
+        uint8_t change  = current ^ state;
+        state = current;
+        if ( (0 != (change & MenuKeys::Key4)) && (0 != (current & MenuKeys::Key4)) ) { if (0x01<mode) mode=mode>>1; }
+        if ( (0 != (change & MenuKeys::Key5)) && (0 != (current & MenuKeys::Key5)) ) {}
+        if ( (0 != (change & MenuKeys::Key6)) && (0 != (current & MenuKeys::Key6)) ) {}
+        if ( (0 != (change & MenuKeys::Key7)) && (0 != (current & MenuKeys::Key7)) ) { if (0x08>mode) mode=mode<<1; }
 
         if (mode!=prev) {
             disp.set( disp.get() ^ mode ^ prev );
             prev = mode;
         }
+
+        // let's do at least lousy debouncing
+        _delay_ms(10); // from <util/delay.h>
     }
-}
-
-
-void main_blank1() {
-    // INITIALIZATION
-    MenuKeys Keys;
-    LEDs     disp;
-    while (1) ;
-}
-
-
-void main_blank2() {
-    // INITIALIZATION
-    MenuKeys Keys;
-    LEDs     disp;
-    while (1) ;
 }
 
 
 int main(void)
 {
-    LEDs diag;
-
-    switch ( choose_on_power_on() ) {
-        case MenuKeys::Key4 :
-        diag.set(LEDs::LED5);
-        main_toggle1();
-        break;
-        case MenuKeys::Key5 :
-        diag.set(LEDs::LED5|LEDs::LED6);
-        main_menu1();
-        break;
-        case MenuKeys::Key6 :
-        diag.set(LEDs::LED5|LEDs::LED7);
-        main_blank1();
-        break;
-        case MenuKeys::Key7 :
-        diag.set(LEDs::LED5|LEDs::LED6|LEDs::LED7);
-        main_blank2();
-        break;
-    }
-
+    main_toggle();
+    // main_menu();
     return(0);
 }
